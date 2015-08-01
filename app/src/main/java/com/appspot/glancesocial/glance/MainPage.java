815cb38adfbca2f6ccb3d635437161b4391353d6
JonahStarling <starling.jonah@gmail.com>
2015-08-01 13:45:30 -0500

    Building the Timeline
    Added a fragment to the main page, added and rearranged some xml files,
    and add dummy usernames to fill in the cards.

--- app/src/main/java/com/appspot/glancesocial/glance/MainPage.java
+++ app/src/main/java/com/appspot/glancesocial/glance/MainPage.java
@@ -2,9 +2,19 @@ package com.appspot.glancesocial.glance;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainPage extends ActionBarActivity {
@@ -14,6 +24,11 @@ public class MainPage extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MainFragment())
+                    .commit();
+        }
         Intent testIntent = new Intent(this, InstagramService.class);
         startService(testIntent);
     }
@@ -43,5 +58,55 @@ public class MainPage extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class MainFragment extends Fragment {
+
+        // TODO:
+            // This only will fill in the usernames with dummy data.
+            // We have to create a custom adapter to fill in all of our data.
+
+        ArrayAdapter<String> mPostAdapter;
+
+        public MainFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            // Create some dummy data for the ListView.
+            String[] userNames = {
+                    "Jonah",
+                    "Christina",
+                    "Daisy",
+                    "Fran",
+                    "Jake",
+                    "Ethan",
+                    "Courtney",
+                    "Shawn",
+                    "Nicole",
+                    "Ross"
+            };
+            List<String>  users = new ArrayList<String>(Arrays.asList(userNames));
+
+            mPostAdapter =
+                    new ArrayAdapter<String>(
+                            getActivity(), // The current context (this activity)
+                            R.layout.card, // The name of the layout ID.
+                            R.id.user_name, // The ID of the textview to populate.
+                            users);
+
+            View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+
+            // Get a reference to the ListView, and attach this adapter to it.
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_posts);
+            listView.setAdapter(mPostAdapter);
+
+            return rootView;
+        }
     }
 }
