e2fc9174ef36a5346c1f6affc51b63a2550f327a
yueqizhang <daisyzhang55@hotmail.com>
2015-08-01 21:53:04 -0400

    finished instagram best friends, small bug fixes

--- app/src/main/java/com/appspot/glancesocial/glance/LoginPageFragment.java
+++ app/src/main/java/com/appspot/glancesocial/glance/LoginPageFragment.java
@@ -3,7 +3,6 @@ package com.appspot.glancesocial.glance;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +12,7 @@ import android.widget.ImageButton;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class LoginPageFragment extends Fragment implements View.OnClickListener{
+public class LoginPageFragment extends Fragment{
     ImageButton instagramLogin;
     public LoginPageFragment() {
     }
@@ -21,16 +20,15 @@ public class LoginPageFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.v(" ", "view created");
-
         View view = inflater.inflate(R.layout.login_fragment, container, false);
         instagramLogin = (ImageButton) view.findViewById(R.id.instagram_login_button);
-        Log.d(" ", "button found");
-        if(instagramLogin == null){
-            Log.d("tag", "button is null");
-        }
-        instagramLogin.setOnClickListener(this);
-        view = inflater.inflate(R.layout.login_fragment, container, false);
+        instagramLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), InstaWebViewActivity.class);
+                startActivity(intent);
+            }
+        });
         Button skipLogin = (Button) view.findViewById(R.id.skip_login);
         skipLogin.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -40,12 +38,5 @@ public class LoginPageFragment extends Fragment implements View.OnClickListener{
             }
         });
         return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Log.d(" ", "Insta clicked");
-        Intent intent = new Intent(getActivity(), InstaWebViewActivity.class);
-        startActivity(intent);
     }
 }
