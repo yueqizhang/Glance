1421670c69b1bcfa339ec91ba355a903479f3bb1
yueqizhang <daisyzhang55@hotmail.com>
2015-08-05 15:15:41 -0400

    bug fixes

--- app/src/main/java/com/appspot/glancesocial/glance/MainPageFragment.java
+++ app/src/main/java/com/appspot/glancesocial/glance/MainPageFragment.java
@@ -1,13 +1,11 @@
 package com.appspot.glancesocial.glance;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
