c5c2b59035bc02894709e2451e74ebc2eb3cd4e2
ctobias44 <tobias.christina@gmail.com>
2015-08-05 11:58:03 -0700

    TwitterContract and dbHelper

--- app/data/GlanceProvider.java
+++
@@ -1,5 +0,0 @@
-package com.appspot.glancesocial.glance;
-
-import android.provider.BaseColumns;
-import android.text.format.Time;
-
--- app/src/main/java/com/appspot/glancesocial/glance/TwitterDBHelper.java
+++ app/src/main/java/com/appspot/glancesocial/glance/TwitterDBHelper.java
@@ -5,25 +5,25 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by yueqizhang on 8/4/15.
+ * Created by christina on 8/5/15.
  */
-public class InstagramDBHelper extends SQLiteOpenHelper {
+public class TwitterDBHelper extends SQLiteOpenHelper {
 
     public static final String TABLE_COMMENTS = "comments";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_COMMENT = "comment";
 
-    private static final String DATABASE_NAME = "instagram.db";
+    private static final String DATABASE_NAME = "twitter.db";
     private static final int DATABASE_VERSION = 1;
 
 
-    public InstagramDBHelper(Context context) {
+    public TwitterDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        final String SQL_CREATE_USER_TABLE = "CREATE TABLE " + InstagramContract.UserEntry.TABLE_NAME + " (" +
+        final String SQL_CREATE_USER_TABLE = "CREATE TABLE " + TwitterContract.UserEntry.TABLE_NAME + " (" +
                 InstagramContract.UserEntry._ID + " INTEGER PRIMARY KEY, " +
                 InstagramContract.UserEntry.COLUMN_USER_NAME + " TEXT UNIQUE NOT NULL, " +
                 InstagramContract.UserEntry.COLUMN_PROFILE_PIC + " TEXT NOT NULL, " +
@@ -31,7 +31,7 @@ public class InstagramDBHelper extends SQLiteOpenHelper {
                 InstagramContract.UserEntry.COLUMN_USER_ID + " INTEGER NOT NULL " +
                 " );";
 
-        final String SQL_CREATE_POST_TABLE = "CREATE TABLE " + InstagramContract.PostEntry.TABLE_NAME + " (" +
+        final String SQL_CREATE_POST_TABLE = "CREATE TABLE " + TwitterContract.PostEntry.TABLE_NAME + " (" +
                 InstagramContract.PostEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 InstagramContract.PostEntry.COLUMN_USER_KEY + " INTEGER NOT NULL, " +
                 InstagramContract.PostEntry.COLUMN_POST_ID + " INTEGER NOT NULL, " +
@@ -45,8 +45,8 @@ public class InstagramDBHelper extends SQLiteOpenHelper {
                 InstagramContract.PostEntry.COLUMN_COMMENTS + " TEXT NOT NULL, " +
                 InstagramContract.PostEntry.COLUMN_LIKES + " INTEGER NOT NULL, " +
                 // Set up the user column as a foreign key to user table.
-                " FOREIGN KEY (" + InstagramContract.PostEntry.COLUMN_USER_KEY + ") REFERENCES " +
-                InstagramContract.UserEntry.TABLE_NAME + " (" + InstagramContract.UserEntry._ID + ") )";
+                " FOREIGN KEY (" + TwitterContract.PostEntry.COLUMN_USER_KEY + ") REFERENCES " +
+                InstagramContract.UserEntry.TABLE_NAME + " (" + TwitterContract.UserEntry._ID + ") )";
 
         db.execSQL(SQL_CREATE_USER_TABLE);
         db.execSQL(SQL_CREATE_POST_TABLE);
@@ -55,8 +55,8 @@ public class InstagramDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + InstagramContract.UserEntry.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + InstagramContract.PostEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TwitterContract.UserEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TwitterContract.PostEntry.TABLE_NAME);
         onCreate(db);
     }
 }
