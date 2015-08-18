ffbdb7ead1c3587232bbc56fa1501b4c6dd6d6b6
JonahStarling <starling.jonah@gmail.com>
2015-08-18 02:37:59 -0500

    Moved Parse Initialization
    Parse is now initialized in the GlanceApp.java file.

--- app/src/main/java/com/appspot/glancesocial/glance/GlanceApp.java
+++ app/src/main/java/com/appspot/glancesocial/glance/GlanceApp.java
@@ -2,6 +2,7 @@ package com.appspot.glancesocial.glance;
 
 import android.app.Application;
 
+import com.parse.Parse;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 
@@ -14,11 +15,15 @@ public class GlanceApp extends Application {
 
     private static final String TWITTER_KEY = "otG6EvQxlUAQmxqmdRmnsOKBi";
     private static final String TWITTER_SECRET = "Eh7kzCtbQHCY0Oxg5E60t0BgSzRLIMZRvthdgZEesAa2m96dY7";
+    private static final String PARSE_APPLICATION_ID = "t7aZ9iBlFtU4JOXtvm2F3Hb5A0zqOXh2uysEeZ3X";
+    private static final String PARSE_CLIENT_KEY = "mUp7lGpKhfhHOuJ0DZCjvJOzDt8wQouo1wWsRHM9";
 
     @Override
     public void onCreate() {
         super.onCreate();
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
     }
 }
